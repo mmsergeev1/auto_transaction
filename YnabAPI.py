7c5d4561c7dcd46c3a98ebee66ff113e04e65276
m.m.sergeev <m.m.sergeev1@gmail.com>
2020-06-26 14:07:05 +0300

    get user info, updated dependencies

---
+++ YnabAPI.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import time
+import ynab
+from ynab.rest import ApiException
+
+
+def get_user(token):
+    # Configure API key authorization: bearer
+    configuration = ynab.Configuration()
+    configuration.api_key['Authorization'] = str(token)
+    configuration.api_key_prefix['Authorization'] = 'Bearer'
+
+    # create an instance of the API class
+    api_instance = ynab.UserApi(ynab.ApiClient(configuration))
+
+    try:
+        # User info
+        api_response = api_instance.get_user()
+        return api_response
+    except ApiException as e:
+        print("Exception when calling UserApi->get_user: %s\n" % e)
---
+++ __main__.py
@@ -0,0 +1,33 @@
+import json
+import YnabAPI
+from os import system, name
+
+
+# define our clear function
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+        # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def print_info():
+    print('© Mikhail Sergeev, 2020, Moscow')
+    print('Read README.md before use')
+    print('===============================')
+
+
+def main():
+    clear()
+    print_info()
+
+    token = str(input("Input personal acess token: "))
+    response_data = YnabAPI.get_user(token)
+    print(response_data)
+
+
+if __name__ == '__main__':
+    main()
